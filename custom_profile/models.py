68515ef1e180ad3d1d1ac72811e1add80aafebf4
YeonGyu <public.kim.yeon.gyu@gmail.com>
2019-11-24 19:31:02 +0900

    Add accessible_beacon_id field to default user model

--- custom_profile/models.py
+++ custom_profile/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE)
+    accessible_beacon_id = models.Textfield(
+        blank=True, Null=False, default="[]")  # formatted as json array
