cfc801860ec03a173309e2a33d5e7649141ff713
YeonGyu <code.yeon.gyu@gmail.com>
2019-11-25 22:21:16 +0900

    Add documentations to app custom_profile

--- custom_profile/serializers.py
+++ custom_profile/serializers.py
@@ -1,8 +1,23 @@
 from rest_framework import serializers
+
+import drf_yasg.openapi as openapi
+
 from custom_profile.models import Profile
 
 
 class ProfileSerializer(serializers.ModelSerializer):
+    accessible_beacon_id = serializers.ReadOnlyField()
+
     class Meta:
         model = Profile
         fields = ('name', 'accessible_beacon_id')
+        parameters = [
+            openapi.Parameter('name',
+                              openapi.IN_QUERY,
+                              description="name",
+                              type=openapi.TYPE_STRING),
+            openapi.Parameter('accessible_beacon_id',
+                              openapi.IN_QUERY,
+                              description="JSON typed integer array",
+                              type="JSON typed-array"),
+        ]
--- custom_profile/views.py
+++ custom_profile/views.py
@@ -2,12 +2,25 @@ import json
 
 from rest_framework import permissions, status
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+
+from drf_yasg.utils import swagger_auto_schema
+import drf_yasg.openapi as openapi
 
 from custom_profile.forms import SignUpForm
 from custom_profile.models import Profile
 from custom_profile.serializers import ProfileSerializer
 
 
+@swagger_auto_schema(method='post',
+                     operation_description="Resgiter a new account",
+                     responses={
+                         201: 'Account Created Successfully',
+                         406: 'Errors occured with given datas'
+                     },
+                     manual_parameters=SignUpForm.Meta.parameters)
+@api_view(['POST'])
 def register(request):  # 회원가입
     form = SignUpForm(request.POST)
     if form.is_valid():
@@ -22,22 +35,28 @@ def register(request):  # 회원가입
 
 
 class ProfileAPIView(APIView):
-    """
-        Profile
-        ---
-        # Content
-            - name: CharField(max_length=10, null=True): name field
-            - accessible_beacon_id: TextField(blank=True, Null=False, default="[]"): json string field
-    """
     permission_classes = (permissions.IsAuthenticated, )
 
-    # queryset = Profile.objects.all()
+    queryset = Profile.objects.all()
 
-    def get(self, request):  # 프로필 조회
+    @swagger_auto_schema(
+        operation_description="Get the informations of requested user",
+        responses={200: 'Successfully returned the requested value.'},
+        manual_parameters=ProfileSerializer.Meta.parameters)
+    def get(self, request):
         profile = Profile.objects.get(user=request.user)
         return Response(ProfileSerializer(profile).data,
                         status=status.HTTP_200_OK)
 
+    @swagger_auto_schema(
+        operation_description="Update the informations of requested user",
+        responses={
+            200: 'Successfully updated the requested value.',
+            406: 'Errors occured with given datas'
+        },
+        manual_parameters=[
+            ProfileSerializer.Meta.parameters[0],
+        ])
     def patch(self, request):
         profile = Profile.objects.get(user=request.user)
         serializer = ProfileSerializer(profile, data=request.data)
