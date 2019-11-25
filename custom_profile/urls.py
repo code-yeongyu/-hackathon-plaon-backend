6d78502ad9ff0792893ebf6b047473c0e79c9aa1
YeonGyu <code.yeon.gyu@gmail.com>
2019-11-25 21:38:25 +0900

    Route custom_profile

--- custom_profile/urls.py
+++ custom_profile/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+import custom_profile.views as views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('account/', include('custom_profile.urls'))
+    path('register/', views.register, name = 'register')
+    path('', views.ProfileAPIView.as_view(), name = 'profileOverall')
 ]
