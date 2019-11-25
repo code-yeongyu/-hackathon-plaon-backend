477694e54f8f714991c81882f10955998b8700ea
YeonGyu <code.yeon.gyu@gmail.com>
2019-11-25 23:25:20 +0900

    Create model Doorlock

--- doorlock/models.py
+++ doorlock/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Doorlock(models.Model):
+    owner = models.ForeignKey(
+        'auth.user',
+        related_name='doorlock_owner',
+        on_delete=models.CASCADE,
+        null=False,
+    )
+    beacon_id = models.IntegerField(null=False, default=-1)
+    push_id = models.IntegerField(null=False, default=-1)
+    location_text = models.Textfield(null=False, default="")
+    nickname = models.CharField(max_length=20, null=False, default="")
