3599d62eec870390fbb8f3d0e333c3ec54d8a9dc
YeonGyu <code.yeon.gyu@gmail.com>
2019-11-25 23:25:51 +0900

    Add Serializer to app doorlock

---
+++ doorlock/serializers.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+
+import drf_yasg.openapi as openapi
+
+from doorlock.models import Doorlock
+
+
+class DoorlockSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    beacon_id = serializers.ReadOnlyField(source='beacon_id')
+    push_id = serializers.ReadOnlyField(source='push_id')
+
+    class Meta:
+        model = Doorlock
+        fields = ('owner', 'beacon_id', 'push_id', 'location_text', 'nickname')
+        parameters = [
+            openapi.Parameter('owner',
+                              openapi.IN_QUERY,
+                              description="username of owner",
+                              type=openapi.TYPE_STRING),  # not for patch
+            openapi.Parameter('beacon_id',
+                              openapi.IN_QUERY,
+                              description="beacon id",
+                              type=openapi.TYPE_NUMBER),  # not for patch
+            openapi.Parameter('push_id',
+                              openapi.IN_QUERY,
+                              description="push id",
+                              type=openapi.TYPE_NUMBER),  # not for patch
+            openapi.Parameter('location_text',
+                              openapi.IN_QUERY,
+                              description="location text",
+                              type=openapi.TYPE_STRING),
+            openapi.Parameter('nickname',
+                              openapi.IN_QUERY,
+                              description="nickname",
+                              type=openapi.TYPE_STRING),
+        ]
