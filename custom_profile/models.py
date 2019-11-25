ec3108a86ab62e66752a18c0ef79b5330351b297
YeonGyu <code.yeon.gyu@gmail.com>
2019-11-25 21:37:37 +0900

    Add name field to custom_profile

--- custom_profile/models.py
+++ custom_profile/models.py
@@ -5,5 +5,6 @@ from django.conf import settings
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL,
                                 on_delete=models.CASCADE)
+    name = models.CharField(max_length=10, null=True)
     accessible_beacon_id = models.Textfield(
         blank=True, Null=False, default="[]")  # formatted as json array
